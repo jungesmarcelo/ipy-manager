af581e43e3a6050921f8f82d465df5514962bc4f
Marcelo Junges <marcelo.junges@byne.com.br>
2021-10-11 09:36:36 -0300

    models and migrations

--- src/ipy_manager_project/ipy_manager_api/models.py
+++ src/ipy_manager_project/ipy_manager_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class network(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  
+    network = models.GenericIPAddressField(protocol='IPv4')
+    create_at = models.DateField(auto_now_add=True)
+    owner = models.TextField(max_length=30)
+    tag  = models.TextField(max_length=10)
+    netmask = models.GenericIPAddressField(protocol='IPv4')
+
+class range(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  
+    network = models.GenericIPAddressField(protocol='IPv4')
+    create_at = models.DateField(auto_now_add=True)
+    owner = models.TextField(max_length=30)
+    tag  = models.TextField(max_length=10)
+
+
+class ip(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  
+    ip = models.GenericIPAddressField(protocol='IPv4')
+    in_use = models.BooleanField(default=False)
+    create_at = models.DateField(auto_now_add=True)
+    owner = models.TextField(max_length=30)
+    tag  = models.TextField(max_length=10)
+    
+    class Meta:
+        ordering = ['-create_at']
+
+
+
